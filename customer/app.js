0fe138bacc220a94142f6ce54eafadc1267cab5f
aonce35 <aonce35@gmail.com>
2026-08-20 20:28:56 +0300

    Update app.js

--- customer/app.js
+++ customer/app.js
@@ -1,3 +1,4 @@
+document.body.innerHTML = "<h1 style='font-size:40px;padding:30px'>APP.JS ÇALIŞIYOR</h1>";
 const SUPABASE_URL = "https://dppqwgsawarkyzzonzyu.supabase.co";
 alert("APP.JS ÇALIŞIYOR");
 const SUPABASE_ANON_KEY = "sb_publishable_fhNovHoi8Xgh1jScRsdrgQ_ZCe6I4tP";
